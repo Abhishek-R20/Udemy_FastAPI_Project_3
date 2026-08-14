d0847ee45517abf854fe0f3316e0b524b5e34d15
Abhishek Raut <abhishekraut695@gmail.com>
2026-08-14 10:39:19 +0530

    Get All ToDos from Database

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from fastapi import FastAPI
-import models
-from database import engine
+from fastapi import FastAPI, Depends
+from models import Base, Todos
+from database import engine, get_db
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.get("/")
 async def home():
     return {"message": "Welcome to project 3"}
+
+
+@app.get("/todos/")
+async def real_all(db: db_dependency):
+    return db.query(Todos).all()
