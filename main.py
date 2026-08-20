3a1994056f6fbb15a7a278eb7bcbbf86b3ea6df5
Abhishek Raut <abhishekraut695@gmail.com>
2026-08-20 08:08:32 +0530

    Get ToDo by ID

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, status, Path
 from models import Base
 import models
 from database import engine, get_db
@@ -23,8 +23,18 @@ async def home():
 #     return db.query(Todos).all()
 
 
-@app.get("/todos/")
+@app.get("/todos/", status_code=status.HTTP_200_OK)
 async def real_all(db: db_dependency):
     result = db.execute(select(models.Todos))
     todos = result.scalars().all()
     return todos
+
+
+# It is a strict rule in the Python language: You cannot put a parameter without a default value after a parameter with a default value.
+@app.get("/todos/get_todo/{id}", status_code=status.HTTP_200_OK)
+async def get_todo_by_id(db: db_dependency, id: Annotated[int, Path(ge=0)]):
+    result = db.execute(select(models.Todos).where(models.Todos.id == id))
+    todo_by_id = result.scalars().first()
+    if todo_by_id is not None:
+        return todo_by_id
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="NO Task Found")
